a63f2b7e7786c618fa43ed828f6fc42198090576
Hysteriaa <moonshowmaker@gmail.com>
2025-06-08 18:29:51 +0800

    Create 1.sol

---
+++ Day5 AdminOnly/1.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AdminOnly {
+    address public owner;
+    uint256 public treasureAmount;
+    mapping(address => uint256) public withdrawalAllowance;
+    mapping(address => bool) public hasWithdrawn;
+
+    constructo
+        owner=msg.sender;
+    
